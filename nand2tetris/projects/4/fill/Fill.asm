33027a3db4bccc0f569685e3d04d27b9bd53a5bb
Michael <178421968+frecrt@users.noreply.github.com>
2026-05-30 18:26:36 +0800

    Instruction Set

--- nand2tetris/projects/4/fill/Fill.asm
+++ nand2tetris/projects/4/fill/Fill.asm
@@ -9,3 +9,54 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+
+// The keyboard memory map is as follows:
+(DETECTKEY)
+@KBD
+D=M
+@DESCRIBE_WHITE
+D;JEQ
+@DESCRIBE_BLACK
+0;JMP
+
+(DESCRIBE_WHITE)
+@SCREEN
+D=A
+@R1
+A=D+M
+M=0
+@R1
+M=M+1
+@8192
+D=A
+@R1
+D=D-M
+@RESET_COUNTER
+D;JEQ
+@DETECTKEY
+0;JMP
+
+(DESCRIBE_BLACK)
+@SCREEN
+D=A
+@R1
+A=D+M
+M=-1
+@R1
+M=M+1
+@8192
+D=A
+@R1
+D=D-M
+@RESET_COUNTER
+D;JEQ
+// 每画一帧就检查一次键盘输入，如果有键被按下，就继续画下一帧，否则就清屏
+@DETECTKEY
+0;JMP
+
+// 重置计数器R1
+(RESET_COUNTER)
+@R1
+M=0
+@DETECTKEY
+0;JMP
--- nand2tetris/projects/4/mult/Mult.asm
+++ nand2tetris/projects/4/mult/Mult.asm
@@ -8,3 +8,31 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+
+
+@R2
+M=0 // R2 = 0
+
+(LOOP)
+@R1
+D=M
+@END
+D;JEQ  // while (R1 != 0)
+
+@R0
+D=M // D = R0
+
+@R2
+M = M + D // R2 = R2 + R0
+
+@R1
+D = M // D = R1
+M = D - 1 // R1 = R1 - 1
+
+
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
